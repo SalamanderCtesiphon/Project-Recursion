e539a56f8a8947d8f7777082b0158b8b9c78a2a6
SalamanderCtesiphon <swbrookshire@gmail.com>
2022-12-26 13:04:17 -0600

    creates a recursive function that takes a number and returns an array containing that many numbers in the fibonacci sequence.

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-let arr = [];
+/* let arr = [0,1];
 
 function fibs(n) {
     arr = [0, 1];
@@ -8,6 +8,19 @@ function fibs(n) {
     return arr;
 }
 
-console.log(fibs(8));
+console.log(fibs(8)); */
 
+//a recursive function that returns an array containing that many numbers of the fibonacci sequence
+
+function fibsRec(n) {
+    if (n === 2) {
+        return [0, 1];
+    } else {
+        let s = fibsRec(n - 1);
+        s.push(s[s.length - 1] + s[s.length - 2]);
+        return s;
+    }
+}
+
+console.log(fibsRec(8));
 
