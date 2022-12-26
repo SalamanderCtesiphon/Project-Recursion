4a770573ab63a20e3db12bc859ecc2a18441251e
SalamanderCtesiphon <swbrookshire@gmail.com>
2022-12-26 12:49:16 -0600

    Created a function which  takes a number and returns an array containg that many numbers from the fibonacci requence.

---
+++ index.js
@@ -0,0 +1,13 @@
+let arr = [];
+
+function fibs(n) {
+    arr = [0, 1];
+    for (let i = 2; i < n; i++) {
+        arr.push(arr[i - 2] + arr[i - 1]);
+    }
+    return arr;
+}
+
+console.log(fibs(8));
+
+
