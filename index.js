b22bf55c3341464f38765a84af0758be8c1f5721
salamanderctesiphon <swbrookshire@gmail.com>
2023-03-30 12:39:26 -0500

    i thought I had a solution but I was wrong

--- index.js
+++ index.js
@@ -1,19 +1,7 @@
-
-function fib(n) {
-  let results = [];
-  if (n == 0) {
-    results = [0];
-  } else if (n == 1) {
-    results = [0, 1];
-  } else {
-    results = [0, 1]
-    for (let i = 2; i < n; i++) {
-      let sum;
-      sum = (i - 1) + (i);
-      results.push(sum);
-    }
-  }
-  return results;
+function fibs(n) {
+  
 }
 
-console.log(fib(8));
+
+
+console.log(fibs(8))
