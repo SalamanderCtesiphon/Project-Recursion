d3941149ebd3253322dc3fc4cfb50e53b5113792
salamanderctesiphon <swbrookshire@gmail.com>
2023-03-30 11:31:53 -0500

    created an array of fibonacci numbers using a for loop

--- index.js
+++ index.js
@@ -1,4 +1,19 @@
 
 function fib(n) {
-  
+  let results = [];
+  if (n == 0) {
+    results = [0];
+  } else if (n == 1) {
+    results = [0, 1];
+  } else {
+    results = [0, 1]
+    for (let i = 2; i < n; i++) {
+      let sum;
+      sum = (i - 1) + (i);
+      results.push(sum);
+    }
+  }
+  return results;
 }
+
+console.log(fib(8));
