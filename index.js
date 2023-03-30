0b1d8b1a004d36e2810531ca68d592b1f4e7852f
salamanderctesiphon <swbrookshire@gmail.com>
2023-03-30 14:44:03 -0500

    added some validation to the functional answer

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
-/* let results = [];
+let results = [];
 
 function fibs(n) {
-  if (n == 1) {
+  if ( n <= 0 ) {
+    return "Please enter a valid number of elements to be given an answer.";
+  } else if (n == 1) {
     results = [0];
   } else if ( n == 2) {
     results = [0, 1];
@@ -20,5 +22,16 @@ function fibs(n) {
   return results;
 }
 
-console.log(fibs(8)) */
+console.log(fibs(8))
 
+/* let results = [];
+function fibsRec(n) {
+  if (n < 2) {
+    return n;
+  } else {
+    return fibsRec(n - 1) + fibsRec(n - 2);
+    
+  }
+}
+
+console.log(fibsRec(7)); */
