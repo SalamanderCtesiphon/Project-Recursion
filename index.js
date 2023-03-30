9b4e631639b49ccd0ddfde26facdb76f2b1042ed
salamanderctesiphon <swbrookshire@gmail.com>
2023-03-30 12:57:03 -0500

    I am lost

--- index.js
+++ index.js
@@ -1,7 +1,22 @@
+let results = [];
+
 function fibs(n) {
-  
+  if (n == 1) {
+    results = [0];
+  } else if ( n == 2) {
+    results = [0, 1];
+  } else {
+    let sum;
+    for (let i = 3; i <= n; i++) {
+      results = [0, 1];
+      sum = (i - 2) + (i - 1);
+      results.push(sum);
+    }
+    
+  }
+  return results;
 }
 
 
 
-console.log(fibs(8))
+console.log(fibs(4))
